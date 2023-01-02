83a557d3d81c54dabd8ec4a2c068fa5d2f30fde1
zs <zs@bjpowernode.com>
2023-01-02 12:11:22 +0800

    000

--- test02.java
+++ test02.java
@@ -1,10 +1,6 @@
 public class test02 {
-
         private String name;
         private String age;
         private String  qq;
         private String dd;
-        private String yj;
-        private String zk;
-        private String yh
 }
